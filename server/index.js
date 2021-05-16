8d700246c99d0ca0d3cb7c591a5564dcf5e2f8d0
Alanna Matieu <alanna.matieu@clw-mb2.na.tuimarine.com>
2021-05-15 21:32:02 -0400

    server created

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const pool = require('./db');
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => {
+    console.log('Server listening on port 5000')
+});
